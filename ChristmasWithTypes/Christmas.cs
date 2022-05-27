7c811b7ebe8fd803f06b6192e899487880909f3e
Baines Deal <bainesdeal@gmail.com>
2022-05-27 14:20:26 -0500

    deleted one stupid curly bracket, and everyone loses their mind

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -5,7 +5,7 @@ namespace ChristmasWithTypes
     {
         public string[] Presents;
         public string Santa { get; set; }
-        public int? TreeHeight { get; set; 
+        public int? TreeHeight { get; set; }
         public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
 
     }
